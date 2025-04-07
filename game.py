e788687ff2a9ed6c5e4d74040718d282b3ebe45b
aylliotthe <eliot.masson@efrei.net>
2025-04-07 09:21:03 +0200

    update

--- game.py
+++ game.py
@@ -7,8 +7,12 @@ from assets import *
 class Game:
     def __init__(self):
         init()
-        self.screen = display.set_mode((TAILLEX,TAILLEY))
+        font.init()
+        self.font = font.SysFont(None, 60)
+
+        self.screen = display.set_mode((TAILLEX, TAILLEY), FULLSCREEN)
         display.set_caption("Jeu")
+
         self.clock = time.Clock()
         self.running = True
         self.fullscreen = False
@@ -22,88 +26,103 @@ class Game:
 
     def afficher_vie(self, joueur):
         if joueur.num == 2:
-            x = 50
-            y = 10
-            k = 1
+            x, y, direction = 50, 10, 1
         else:
-            x = TAILLEX - 50
-            y = 10
-            k = -1
+            x, y, direction = TAILLEX - 50, 10, -1
 
-        nb_vie = joueur.life
-
-        for i in range(nb_vie//2):
+        for _ in range(joueur.life // 2):
             self.screen.blit(coeurImage, (x, y))
-            x += k * 50
+            x += direction * 50
 
-        nb_vie = nb_vie % 2
-
-        for i in range(nb_vie):
+        if joueur.life % 2:
             self.screen.blit(demicoeurImage, (x, y))
-            x += k * 50
+
+    def afficher_pause(self):
+        paused = True
+        bouton_menu = Rect(TAILLEX // 2 - 150, TAILLEY // 2 - 50, 300, 100)
+
+        while paused:
+            for event_pause in event.get():
+                if event_pause.type == QUIT:
+                    self.running = False
+                    return
+                if event_pause.type == KEYDOWN and event_pause.key == K_ESCAPE:
+                    return
+                if event_pause.type == MOUSEBUTTONDOWN and bouton_menu.collidepoint(event_pause.pos):
+                    self.running = False
+                    return
+
+            draw.rect(self.screen, (200, 0, 0), bouton_menu)
+            texte = self.font.render("Menu principal", True, (255, 255, 255))
+            self.screen.blit(texte, (bouton_menu.x + 30, bouton_menu.y + 30))
+            display.flip()
+            self.clock.tick(15)
 
     def run(self):
-        plateforme1 = PlateformeRect()
-        plateforme2 = PlateformeRect(300,50, TAILLEX / 2 -75, TAILLEY/3 -50)
-        plateforme3 = PlateformeRect(300,25, (TAILLEX / 6) -75, TAILLEY/2 -25)
-        plateforme4 = PlateformeRect(300,25, TAILLEX - ((TAILLEX / 6) -100), TAILLEY/2-25)
+        # Plateformes
+        self.all_plateforme.add(
+            PlateformeRect(),
+            PlateformeRect(300, 50, TAILLEX / 2 - 75, TAILLEY / 3 - 50),
+            PlateformeRect(300, 25, (TAILLEX / 6) - 75, TAILLEY / 2 - 25),
+            PlateformeRect(300, 25, TAILLEX - ((TAILLEX / 6) - 100), TAILLEY / 2 - 25)
+        )
 
-        self.all_plateforme.add(plateforme1, plateforme2,plateforme3,plateforme4)
+        # Joueurs
+        joueur1 = Player(kittyImage, 2, self.projectiles_joueur1, "Assets/Personnage/CharaKitty/KittyProjo.png")
+        joueur2 = Player(messiImage, 1, self.projectiles_joueur2, "Assets/Personnage/CharaMessi/MessiProjo.png")
 
-        joueur1 = Player(kittyImage,2,self.projectiles_joueur1,"Assets/Personnage/CharaKitty/KittyProjo.png")
-        self.all_players.add(joueur1)
+        self.all_players.add(joueur1, joueur2)
         self.grp_1.add(joueur1)
-        joueur2 = Player(messiImage,1,self.projectiles_joueur2,"Assets/Personnage/CharaMessi/MessiProjo.png")
-        self.all_players.add(joueur2)
         self.grp_2.add(joueur2)
 
         while self.running:
-
-            if joueur1.life <= 0:
-                joueur1.kill()
-            if joueur2.life <= 0:
-                joueur2.kill()
-
             for e in event.get():
                 if e.type == QUIT:
                     self.running = False
-                if e.type == KEYDOWN:
-                    if e.key == K_f: 
+                elif e.type == KEYDOWN:
+                    if e.key == K_f:
                         self.fullscreen = not self.fullscreen
                         if self.fullscreen:
                             self.screen = display.set_mode((0, 0), FULLSCREEN)
                         else:
                             self.screen = display.set_mode((TAILLEX, TAILLEY))
+                    elif e.key == K_ESCAPE:
+                        self.afficher_pause()
 
+            # Gestion des vies
+            if joueur1.life <= 0:
+                joueur1.kill()
+            if joueur2.life <= 0:
+                joueur2.kill()
+
+            # Collisions projectiles
             for projectile in self.projectiles_joueur1:
-                collisions = sprite.spritecollide(projectile,self.grp_2, False, sprite.collide_mask)
-                if collisions:
+                if sprite.spritecollide(projectile, self.grp_2, False, sprite.collide_mask):
                     projectile.kill()
                     joueur2.degat_faible()
 
             for projectile in self.projectiles_joueur2:
-                collisions = sprite.spritecollide(projectile,self.grp_1, False, sprite.collide_mask)
-                if collisions:
+                if sprite.spritecollide(projectile, self.grp_1, False, sprite.collide_mask):
                     projectile.kill()
                     joueur1.degat_faible()
 
-            self.all_players.update( self.all_plateforme)
+            # Updates
+            self.all_players.update(self.all_plateforme)
             self.projectiles_joueur1.update()
             self.projectiles_joueur2.update()
 
-            self.screen.fill((0,0,0))
+            # Affichage
+            self.screen.fill((0, 0, 0))
             self.all_plateforme.draw(self.screen)
             self.projectiles_joueur1.draw(self.screen)
             self.projectiles_joueur2.draw(self.screen)
             self.all_players.draw(self.screen)
-            
+
             self.afficher_vie(joueur1)
             self.afficher_vie(joueur2)
 
-        
             display.flip()
             self.clock.tick(FRAMERATE)
-
 
         quit()
         exit()
